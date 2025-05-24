eab4af064a1610a658088735dcb99badfe0c0c7f
kang hee yong <cagojeiger@naver.com>
2025-05-24 14:01:09 +0900

    feat: rename helm command to helm-local

--- src/cli_onprem/commands/__init__.py
+++ src/cli_onprem/commands/__init__.py
@@ -1,5 +1,5 @@
 """Command modules for CLI-ONPREM."""
 
-from . import docker_tar, fatpack, helm, s3_share
+from . import docker_tar, fatpack, helm_local, s3_share
 
-__all__ = ["docker_tar", "fatpack", "helm", "s3_share"]
+__all__ = ["docker_tar", "fatpack", "helm_local", "s3_share"]
